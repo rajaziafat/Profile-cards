ae661ba94b876190158d41a075b8695eeba77a88
Ali Aalam <aliaalam.me@gmail.com>
2024-02-19 23:45:04 +0500

    asd

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,27 +1,34 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+
+
 
 function Card({ name, imageUrl, role, location }) {
     return (
-<div className="flex items-center gap-4 mt-5  backdrop-blur-lg bg-white bg-opacity-10 px-4 py-3 rounded-xl border border-white border-opacity-20">
+        <div className="relative mt-5  backdrop-blur-lg bg-white bg-opacity-10 rounded-xl border border-white border-opacity-20">
+        <div className="absolute top-0 right-2 ">
+            <FontAwesomeIcon icon={faTimes} className="text-white cursor-pointer py-2" />
+        </div>
+        <div className="flex items-center gap-4 px-4  py-4 mt-2">
             <img
                 className="max-w-[60px] max-h-[60px] rounded-full"
                 src={imageUrl}
                 alt=""
             />
-
-            <hr className="border-l border-solid h-12 " />
-
-            <div className="font-[400]  leading-[16px]">
-                <h1 className=" text-white text-[0.9rem]">{name}</h1>
+            <hr className="border-l border-solid h-12" />
+            <div className="font-[400] leading-[16px]">
+                <h1 className="text-white text-[0.9rem]">{name}</h1>
                 <p className="text-[0.9rem] text-gray-500 dark:text-gray-400">
                     {role}
                 </p>
-                <p className=" text-[0.9rem] text-gray-500 dark:text-gray-400">
+                <p className="text-[0.9rem] text-gray-500 dark:text-gray-400">
                     {location}
                 </p>
-                <hr className="font-bold mt-2 " />
+                <hr className="font-bold mt-2" />
             </div>
-
         </div>
+    </div>
+    
     );
 }
 
