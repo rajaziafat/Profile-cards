ad0b86023f3859760a6b2ff71313117ad1905996
Ali Aalam <aliaalam.me@gmail.com>
2024-02-19 15:44:15 +0500

    ssd

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,14 +3,14 @@ function Card({ name, imageUrl, role, location }) {
     return (
         <div className="flex items-center gap-4 mt-5 ">
             <img
-                className="max-w-[100px] max-h-[100px] rounded-full"
+                className="max-w-[75px] max-h-[75px] rounded-full"
                 src={imageUrl}
                 alt=""
             />
 
             <hr className="border-l border-solid h-12 " />
 
-            <div className="font-medium text-xl sm:text-2xl">
+            <div className="font-medium text-xl sm:text-xl">
                 <div>{name}</div>
                 <div className="text-base sm:text-lg text-gray-500 dark:text-gray-400">
                     {role}
